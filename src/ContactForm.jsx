06b324bc62c1220c518942a50cf7256f44da38d2
moskal-alicja <moskal.ala@gmail.com>
2019-11-24 22:08:14 +0100

    Lab7Ex3 Email and phone number validation

--- src/ContactForm.jsx
+++ src/ContactForm.jsx
@@ -9,7 +9,9 @@ class ContactForm extends React.Component {
           email: "",
           parentName: "",
           phoneNumber: "",
-          isAdult: true
+          isAdult: true,
+          notValidPhoneNumber: false,
+          notValidEmail: false,
         };
         this.ageChanged = this.ageChanged.bind(this);
         this.dataChanged = this.dataChanged.bind(this);
@@ -27,15 +29,35 @@ class ContactForm extends React.Component {
         this.setState({[name]: val});
     }
 
-    handleSubmit() {
-        alert("Submit clicked!");
+    handleSubmit(event) {
+        event.preventDefault();
+        if(this.state.isAdult) {
+            const email = this.state.email;
+            const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/g;
+            if(!email.match(emailRegex)) {
+                this.setState({notValidEmail: true});
+                return;
+            }
+            else this.setState({notValidEmail: false});
+        }
+        else {
+            const phoneNumber = this.state.phoneNumber;
+            const numberRegex = /^[0-9]{9}$/g;
+            if(!phoneNumber.match(numberRegex)){
+                this.setState({notValidPhoneNumber: true});
+                return;
+            }
+            else this.setState({notValidPhoneNumber: false});          
+        }
+        
+        
     }
 
 
     render(){
         const isAdult = this.state.isAdult;
         return(            
-            <form onSubmit={this.handleSubmit}>
+            <form onSubmit={this.handleSubmit} >
                 <h3>Form:</h3>
                 <label>
                 Age: <input type="number" value={this.state.age} onChange={this.ageChanged}/>
@@ -44,11 +66,12 @@ class ContactForm extends React.Component {
                 {isAdult ? 
                 <div>
                     <label>
-                        Name: <input type="text" value={this.state.name} name="name" onChange={this.dataChanged}/>
+                        Name: <input type="text" value={this.state.name} name="name" onChange={this.dataChanged}/>                  
                     </label>
                     <br />
                     <label>
                         Email: <input type="text" value={this.state.email} name="email" onChange={this.dataChanged}/>
+                        {this.state.notValidEmail && <label style={{color: "red"}}>Not valid email address!</label>}
                     </label>
                 </div> 
                 : 
@@ -59,10 +82,11 @@ class ContactForm extends React.Component {
                     <br />
                     <label>
                         Parent Phone No: <input type="text" value={this.state.phoneNumber} name="phoneNumber" onChange={this.dataChanged}/>
+                        {this.state.notValidPhoneNumber && <label style={{color: "red"}}>Not valid phone number!</label>}
                     </label>
                 </div> 
                 }
-                <input type="submit" value="Submit" />
+                <input type="submit" value="Submit"/>
             </form>
         );
     }
